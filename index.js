9203f0b50b1ba4e76eb4c19b6d303ba925ae062a
ClaytonDewey <clay@dryadmedia.com>
2025-03-18 12:10:12 -0500

    install and require cors

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const cors = require('cors');
 
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Clayton!');
+  res.send('Hello world!!!!!!');
 });
 
 io.on('connection', (socket) => {
